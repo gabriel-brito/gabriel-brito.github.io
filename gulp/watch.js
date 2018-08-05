45e31c6b9e622185b00c7c65c3c392c03dd4a85e
Gabriel <mfkrgabriel@gmail.com>
2018-08-05 14:49:19 -0300

    Backing to normal

--- gulp/watch.js
+++
@@ -1,9 +0,0 @@
-import gulp from 'gulp';
-import watch from 'gulp-watch';
-
-gulp.task('watch', () => {
-  return gulp.watch(
-    ['./source/**/**/*.styl', './source/**/**/*.js'],
-    ['build']
-  )
-});
