3387612c1e52f9d42ebf39717f438173435d4097
Gabriel Santos <gabrielsantos@ext.cvccorp.com.br>
2019-04-29 16:13:21 -0300

    testing

---
+++ mind-reading/precache-manifest.f64b5cab6511cf1afde5077766d615cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e4d52720e2e2da6b23301078248452",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d6814b6fe3696531cfe",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "0e268b2d45483885f36a",
+    "url": "/static/css/main.3094e3cf.chunk.css"
+  },
+  {
+    "revision": "6d6814b6fe3696531cfe",
+    "url": "/static/js/2.6b0e8f1e.chunk.js"
+  },
+  {
+    "revision": "0e268b2d45483885f36a",
+    "url": "/static/js/main.cec7a4a2.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
